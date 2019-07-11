ad1229fab7539b3f195043fae7f7d362994ee7a8
Matt Rees <matthew.rees@ee.co.uk>
2019-07-11 17:51:24 +0100

    Removed duplicate script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install xcodegen
-xcodegen generate
 sudo gem install bundler
 bundle install
 bundle exec fastlane install_plugins
