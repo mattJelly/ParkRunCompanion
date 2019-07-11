0032d9956e723ddfd65214b2046f8af8cd950d5f
Matt Rees <matthew.rees@ee.co.uk>
2019-07-11 17:49:41 +0100

    Fixed missing xcodegen

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install xcodegen
+xcodegen generate
 sudo gem install bundler
 bundle install
 bundle exec fastlane install_plugins
