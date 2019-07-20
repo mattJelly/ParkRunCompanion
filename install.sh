7ba8ef6bb8e92d1c74e4dbd1597c656f78f58fa9
Matt <mattterees@gmail.com>
2019-07-20 11:40:32 +0100

    Getting annoyed with homebrew

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew install xcodegen
-sudo gem install bundler
+gem install bundler
 bundle install
 xcodegen generate
 bundle exec fastlane install_plugins
